2b85227682cdafa1ad8802c8eec8c2b4f4bf846c
Marvin Bidzana <marvinbidzana@gmail.com>
2025-06-30 21:44:51 +0200

    Initialisation du projet

---
+++ rendora-backend/src/main/java/com/bidzana/rendora/RendoraApplication.java
@@ -0,0 +1,13 @@
+package com.bidzana.rendora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendoraApplication.class, args);
+	}
+
+}
---
+++ rendora-backend/src/test/java/com/bidzana/rendora/RendoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidzana.rendora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
